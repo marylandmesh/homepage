02d73c206d283663b4f0bea5833cbeeae1b8b4e5
Luke Evers <me@lukevers.com>
2013-01-18 11:17:59 -0500

    Wrote a short-ass thing for getting the most recent post.

--- index.php
+++ index.php
@@ -9,7 +9,32 @@
     	<div class="center">
         	<?php require("header.php"); ?>
             <div class="wrap">
-            	hmm.
+            	<?php
+					include_once "markdown.php";
+					$files = glob('blog/posts/*.{md}', GLOB_BRACE);
+					foreach (array_reverse($files) as $file) {
+						$name = substr($file, 6,-3);
+						$contents = file_get_contents($file);
+						
+						$lines = explode("\n", $contents);
+						$date = $lines[0];
+						$time = $lines[1];
+						$author = $lines[2];
+
+						$lines[0] = "";
+						$lines[1] = "";
+						$lines[2] = "";
+						$contents = implode("\n", $lines);
+						
+						echo '<div class="post" id="'.$name.'">';
+						echo '<div class="date">'.$date.'</div>';
+						echo '<div class="time">'.$time.'</div>';
+						echo Markdown($contents);
+						echo '<div class="author">Posted by '.$author.'.</div>';
+						echo '</div>';
+						break;
+					}
+				?>
             </div>
             <hr>
             <?php require("footer.php"); ?>
