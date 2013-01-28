3e1b862335dbcefc8289c77c1d9922e9c3ec2f0c
Luke Evers <me@lukevers.com>
2013-01-28 18:59:54 -0500

    Rearranged the sidebars and middle bar.

--- index.php
+++ index.php
@@ -8,36 +8,6 @@
     <body>
     	<div class="center">
         	<?php require("header.php"); ?>
-            <div class="wrap">
-            	<?php
-					include_once "blog/markdown.php";
-					$files = glob('blog/posts/*.{md}', GLOB_BRACE);
-					foreach (array_reverse($files) as $file) {
-						$name = substr($file, 6,-3);
-						$contents = file_get_contents($file);
-						
-						$lines = explode("\n", $contents);
-						$date = $lines[0];
-						$time = $lines[1];
-						$author = $lines[2];
-
-						$lines[0] = "";
-						$lines[1] = "";
-						$lines[2] = "";
-						$contents = implode("\n", $lines);
-						
-						echo '<div class="post" id="'.$name.'">';
-						echo '<div class="date">'.$date.'</div>';
-						echo '<div class="time">'.$time.'</div>';
-						echo Markdown($contents);
-						echo '<div class="author">Posted by '.$author.'.</div>';
-						echo '</div>';
-						break;
-					}
-				?>
-            <hr>
-            <?php require("footer.php"); ?>
-            </div>
             <div class="sidebar_l">
             	<div class="sidebar">
                 	<hr>
@@ -76,6 +46,36 @@
 						<a href="/faq.php"><li>FAQ</li></a>
                     </ul>
                 </div>
+            </div>
+            <div class="wrap">
+            	<?php
+					include_once "blog/markdown.php";
+					$files = glob('blog/posts/*.{md}', GLOB_BRACE);
+					foreach (array_reverse($files) as $file) {
+						$name = substr($file, 6,-3);
+						$contents = file_get_contents($file);
+						
+						$lines = explode("\n", $contents);
+						$date = $lines[0];
+						$time = $lines[1];
+						$author = $lines[2];
+
+						$lines[0] = "";
+						$lines[1] = "";
+						$lines[2] = "";
+						$contents = implode("\n", $lines);
+						
+						echo '<div class="post" id="'.$name.'">';
+						echo '<div class="date">'.$date.'</div>';
+						echo '<div class="time">'.$time.'</div>';
+						echo Markdown($contents);
+						echo '<div class="author">Posted by '.$author.'.</div>';
+						echo '</div>';
+						break;
+					}
+				?>
+            <hr>
+            <?php require("footer.php"); ?>
             </div>
         </div>        
     </body>
