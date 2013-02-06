7338248aa8a613de1783d78e4b94e76034023391
Luke Evers <me@lukevers.com>
2013-02-06 18:05:42 -0500

    Fixed footer links

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
             <div class="footer">
                 <a href="/">Home</a> &bull;
                 <a href="/blog/">Blog</a> &bull;
-                <a href="/faq.php">FAQ</a> &bull;
-                <a href="/contact.php">Contact</a> &bull;
+                <a href="/faq/">FAQ</a> &bull;
+                <a href="/contact/">Contact</a> &bull;
                 <a href="http://maryland.ninux.org/">Nodeshot</a> &bull;
                 <a href="/map/">Network Map</a>
             </div>
