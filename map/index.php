609f76c6a7a7a3fc2ac04bde7aedc98a2da7b3ff
Luke Evers <me@lukevers.com>
2013-02-06 18:44:00 -0500

    Added links to each map.

--- map/index.php
+++ map/index.php
@@ -1,8 +1,8 @@
 <?php require("../header.php"); ?>
 
 	<div class="maps">
-    	<img src="maryland-map.svg" class="map"/>
-        <img src="network-map.svg" class="map"/>
+    	<a href="maryland-map.svg"><img src="maryland-map.svg" class="map"/></a>
+        <a href="network-map.svg"><img src="network-map.svg" class="map"/></a>
     </div>
 
 <?php require("../footer.php"); ?>
