1fef649d84269d2e77b95682b8d96483030c288e
Luke Evers <me@lukevers.com>
2013-02-06 20:22:09 -0500

    test

--- map/index.php
+++ map/index.php
@@ -1,8 +1,1 @@
-<?php require("../header.php"); ?>
-
-	<div class="maps">
-    	<a href="maryland-map.svg"><img src="maryland-map.svg" class="map" alt="Maryland Network Map"/></a>
-        <a href="network-map.svg"><img src="network-map.svg" class="map" alt="Complete Network Map"/></a>
-    </div>
-
-<?php require("../footer.php"); ?>
+test
