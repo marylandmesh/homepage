8d802af8b1d48ac4395ef29d483667351425a259
Luke Evers <me@lukevers.com>
2013-02-16 19:37:27 -0500

    Changed the link to the map.
    Nodeshot is now hosted on the same
    server as the homepage.

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
     <a href="/blog/">Blog</a> &bull;
     <a href="/faq/">FAQ</a> &bull;
     <a href="/contact/">Contact</a> &bull;
-    <a href="http://maryland.ninux.org/">Nodeshot</a> &bull;
+    <a href="http://map.maryland.projectmeshnet.org/">Map</a> &bull;
     <a href="http://machupovir.us/">Statistics</a>
 </div>
