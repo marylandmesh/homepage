b5792d0bf1b007bc0b13e2bda7dd022410236330
Luke Evers <me@lukevers.com>
2013-02-18 22:13:39 -0500

    Fixed the projectmeshnet link for the nodeshot map

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                     	<a href="http://www.reddit.com/r/marylandmesh/"><li>/r/marylandmesh</li></a>
                         <a href="https://wiki.projectmeshnet.org/Maryland_Mesh"><li>Wiki</li></a>
                         <a href="https://plus.google.com/communities/100874292106037502249"><li>Google+</li></a>
-                        <a href="http://maryland.ninux.org/"><li>Nodeshot</li></a>
+                        <a href="http://map.maryland.projectmeshnet.org/"><li>Nodeshot</li></a>
                         <hr>
 						<a href="/contact/"><li>Contact Us</li></a>
 						<a href="/faq/"><li>FAQ</li></a>
