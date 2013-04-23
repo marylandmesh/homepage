e1c5c47a1fcb841c139efcb9c99b30a201e0afde
Luke Evers <me@lukevers.com>
2013-04-23 09:20:06 -0400

    Removed old php files

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-    <a href="/">Home</a> &bull;
-    <a href="/blog/">Blog</a> &bull;
-    <a href="/faq/">FAQ</a> &bull;
-    <a href="/contact/">Contact</a> &bull;
-    <a href="http://map.maryland.projectmeshnet.org/">Nodeshot</a> &bull;
-    <a href="http://stats.hyperboria.net/">Statistics</a>
-</div>
--- header.php
+++
@@ -1,3 +0,0 @@
-<title>Maryland Meshnet</title>
-<link rel="stylesheet" href="/css/style.css">
-<link rel="shortcut icon" href="/img/icon_16.png">
