f36fa52b1ea88f279330a7a091008649875f9716
Luke Evers <me@lukevers.com>
2013-04-26 15:52:25 -0400

    Added basic templates-kind of.
    Added simple to understand HTML of what goes where
    when @SashaCrofter writes the information for each
    section for the homepage.
    
    Also, @SashaCrofter, you can add more sections but
    just make sure you use the same format so it stays
    pretty and everything!

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 //
 //	common.js
 //
+
+$(document).ready(function(){
+	// Hide every instance of more
+	$(".more").hide();
+	
+	// Hide/unhide every instance of more
+	$(".readmore").click(function() {
+		$(this).next().slideToggle(500);
+	});
+	
+});
